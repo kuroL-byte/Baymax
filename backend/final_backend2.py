0cc863854d32a066ffca7bf0c5c350707c9d5e57
kuroL-byte <79972311+kuroL-byte@users.noreply.github.com>
2024-05-03 06:52:23 +0530

    www

--- backend/final_backend2.py
+++ backend/final_backend2.py
@@ -1,23 +1,13 @@
-import subprocess
-import wolframalpha
 import pyttsx3
 import pywhatkit
-import tkinter
 import json
-import random
-import operator
 import speech_recognition as sr
 import datetime
 import wikipedia
 import webbrowser
 import os
-import winshell
 import pyjokes
-import feedparser
-import smtplib
 import ctypes
-import time
-import requests
 import shutil
 from twilio.rest import Client
 from clint.textui import progress
